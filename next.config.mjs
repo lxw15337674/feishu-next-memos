7814e0940e255b928ccd305853c0df1911be7b32
bhwa233 <404174262@qq.com>
2024-10-14 18:09:59 +0800

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
-
 import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
     dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development'
 });
 
 export default withPWA({
