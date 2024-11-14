2b2484a07fcad3b58ea0e53248fbd5fd5a9d589a
bhwa233 <404174262@qq.com>
2024-11-14 10:47:25 +0800

    feat: 更新Next.js配置，启用serverActions并设置bodySizeLimit

--- next.config.mjs
+++ next.config.mjs
@@ -6,35 +6,12 @@ const nextConfig = {
             fullUrl: true,
             hmrRefreshes: true,
         },
-    }, // https://nextjs.org/docs/api-reference/next.config.js/logging
-    // async rewrites() {
-    //     return [
-    //         {
-    //             source: '/upload',
-    //             destination: 'https://telegraph-image-bww.pages.dev/upload'
-    //         }
-    //     ]
-    // },
-    // async headers() {
-    //     return [
-    //         {
-    //             source: '/:path*{/}?',
-    //             headers: [
-    //                 { key: 'Access-Control-Allow-Credentials', value: 'true' },
-    //                 { key: 'Access-Control-Allow-Origin', value: '*' },
-    //                 {
-    //                     key: 'Access-Control-Allow-Methods',
-    //                     value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT'
-    //                 },
-    //                 {
-    //                     key: 'Access-Control-Allow-Headers',
-    //                     value:
-    //                         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    //                 }
-    //             ]
-    //         }
-    //     ]
-    // },
+    },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '20mb' // Set desired value here
+        }
+    }
 };
 
 export default nextConfig;
